f9f9a26cf5f63b037c98a61edefb7e8c760bcd0a
Maksim Ahramovich <fromen@yandex-team.ru>
2016-01-19 23:12:15 +0300

    Rename .sql file

--- src/main/java/com/solvegen/test/dao/DaoContextConfiguration.java
+++ src/main/java/com/solvegen/test/dao/DaoContextConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -24,7 +22,7 @@ public class DaoContextConfiguration {
         return new EmbeddedDatabaseBuilder()
                 .setName("dbsolvegen")
                 .setType(EmbeddedDatabaseType.HSQL)
-                .addScript("create-db.sql")
+                .addScript("create-tables.sql")
                 .build();
     }
 
