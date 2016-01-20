b749d5d492b2ab584cc54c40c035d661ae7a0aa5
Maksim Ahramovich <fromen@yandex-team.ru>
2016-01-20 11:08:44 +0300

    Add equals and hashCode to Book class

--- src/main/java/com/solvegen/test/dao/Book.java
+++ src/main/java/com/solvegen/test/dao/Book.java
@@ -35,4 +35,34 @@ public class Book {
                 ", description='" + description + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        if (description != null ? !description.equals(book.description) : book.description != null) return false;
+        if (genre != null ? !genre.equals(book.genre) : book.genre != null) return false;
+        if (id != null ? !id.equals(book.id) : book.id != null) return false;
+        if (price != null ? !price.equals(book.price) : book.price != null) return false;
+        if (publishDate != null ? !publishDate.equals(book.publishDate) : book.publishDate != null) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (publishDate != null ? publishDate.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
