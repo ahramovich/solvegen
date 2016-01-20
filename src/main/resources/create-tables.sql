097286cabe60b0c9ec9b5dec6d308358cc6078fa
Maksim Ahramovich <fromen@yandex-team.ru>
2016-01-20 11:07:49 +0300

    Fix create table script to hsqldb specific

--- src/main/resources/create-tables.sql
+++ src/main/resources/create-tables.sql
@@ -1,11 +1,10 @@
 CREATE TABLE books (
-    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INTEGER IDENTITY PRIMARY KEY,
     book_id VARCHAR(20),
     author VARCHAR(255),
     title VARCHAR(255),
     genre VARCHAR(80),
     price DOUBLE,
     publish_date VARCHAR(20),
-    description VARCHAR(512),
-    PRIMARY KEY (id)
+    description VARCHAR(512)
 );
