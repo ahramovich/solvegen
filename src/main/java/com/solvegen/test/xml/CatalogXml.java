35312c7a9303135f4574b45847effae10c789d7f
Maksim Ahramovich <fromen@yandex-team.ru>
2016-01-20 11:12:17 +0300

    Delete unused imports

--- src/main/java/com/solvegen/test/xml/CatalogXml.java
+++ src/main/java/com/solvegen/test/xml/CatalogXml.java
@@ -1,8 +1,6 @@
 package com.solvegen.test.xml;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
